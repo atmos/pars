0db72107135b07e454c3390e60be43064c469af3
Corey Donohoe <atmos@corey-donohoes-macbook-air.local>
2008-05-25 04:10:56 -0700

    try again

--- spec/lvm_backup_spec.rb
+++ spec/lvm_backup_spec.rb
@@ -1,10 +1,20 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe LvmBackup, "parsing" do
+  describe "literals" do
+    parser = LvmBackupParser.new
+    if parser.parse('zomg')
+      pp parser
+      puts "success"
+    else
+      pp parser
+      puts "FAIL"
+    end
+  end
   it "should do it big like the rappers" do
     parser = LvmBackupParser.new
     pp parser
-    if parser.parse("foo = 'bar'")
+    if parser.parse('foo { bar }')
       pp parser
       puts "success"
     else
