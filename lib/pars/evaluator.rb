4644143c34bccda3079879698cc064291b320611
Corey Donohoe <atmos@corey-donohoes-macbook-air.local>
2008-05-25 02:45:57 -0700

    first round

--- lib/pars/evaluator.rb
+++ lib/pars/evaluator.rb
@@ -1,9 +1,16 @@
 class Evaluator
+  attr_reader :keys
   def initialize(string)
+    @keys = { }
     eval(string)
     local_variables.reject { |var| var == 'string' }.each do |meth|
       self.class.send(:attr_reader, meth.to_sym)
       instance_variable_set("@#{meth}", eval(meth))
+    end
+  end
+  def method_missing(name, *args, &block)
+    if block_given?
+      @keys[name.to_sym] = block.call
     end
   end
 end
--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -11,6 +11,11 @@ describe Evaluator, "do it big like the rappers" do
     @test.should respond_to(:version)
   end
   it "should have two instance variables" do
-    @test.instance_variables.should have(2).things
+    @test.instance_variables.should have(3).things
+  end
+  
+  it "should handle the first round of weird shit i throw at it" do
+    @test = Evaluator.new("hello { i = 0 }")
+    @test.keys[:hello] = { :i => 0 }
   end
 end
