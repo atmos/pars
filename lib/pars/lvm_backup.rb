44ca1d7ddcd30d95606862abe7563ac5d78245c8
Corey Donohoe <atmos@corey-donohoes-macbook-air.local>
2008-05-26 10:55:52 -0700

    fix the require path so the specs run again

--- lib/pars/lvm_backup.rb
+++ lib/pars/lvm_backup.rb
@@ -1,2 +1,2 @@
-Treetop.load File.dirname(__FILE__) + '/pars/lvm_backup/lvm_backup'
-require File.dirname(__FILE__) + '/pars/lvm_backup/lvm_backup_node_classes'
+Treetop.load File.dirname(__FILE__) + '/lvm_backup/lvm_backup'
+require File.dirname(__FILE__) + '/lvm_backup/lvm_backup_node_classes'
