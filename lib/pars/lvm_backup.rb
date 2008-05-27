25262ff443698224c8965db9449c50e43c2a7f23
atmos <atmos@atmos.org>
2008-05-26 20:29:16 -0600

    move things around to be more explicit about whether you should use them or not

--- lib/pars/lvm_backup.rb
+++ lib/pars/lvm_backup.rb
@@ -1,8 +1,9 @@
 Treetop.load File.dirname(__FILE__) + '/lvm_backup/lvm_backup_file_you_shouldnt_use'
-require File.dirname(__FILE__) + '/lvm_backup/lvm_backup_node_classes'
+require File.dirname(__FILE__) + '/lvm_backup/lvm_backup_file_you_shouldnt_use_node_classes'
 
 module Pars
   module LvmBackup
+    # override the LvmBackupFileYouShouldntUseParser class by defining a namespaced one
     class Parser < Treetop::Runtime::CompiledParser
       include LvmBackupFileYouShouldntUse
     end
