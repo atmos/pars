30632722f145d197f8994eece5c282c79de904fd
atmos <atmos@atmos.org>
2008-06-28 14:04:38 -0600

    keepalived parser now lives in its own gem

--- lib/pars.rb
+++ lib/pars.rb
@@ -5,6 +5,5 @@ module Pars; end
 require File.dirname(__FILE__) + '/pars/lvm_backup'
 require File.dirname(__FILE__) + '/pars/xm'
 require File.dirname(__FILE__) + '/pars/monit'
-require File.dirname(__FILE__) + '/pars/keepalived'
 require File.dirname(__FILE__) + '/pars/nginx'
 require File.dirname(__FILE__) + '/pars/xen'
