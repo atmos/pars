ceab4f0c842ba49ebe6013db69160bcdf694e937
atmos <atmos@atmos.org>
2008-06-28 12:38:55 -0600

    couple more small things

--- lib/pars/xen/ast.rb
+++ lib/pars/xen/ast.rb
@@ -1,30 +1,40 @@
 module XenConfigFile
   module AST
+    
     class ConfigFile
       attr_accessor :disks, :vars, :comments
       def initialize(contents)
         @vars = [ ]
         @comments = contents.delete(:comments)
-        disks = contents[:variables].detect { |var| var.lhs == :disk }
-        # pp contents
-        @disks = Disk.build(contents[:variables].delete(disks))
+        @disks = Disk.build(contents)
         
         contents[:variables].each do |var|
           @vars << var
         end
       end
+      
       def [](key)
         @vars.detect { |v| v.lhs == key }.rhs
+      end
+      
+      def []=(key, value)
+        @vars << (value.kind_of?(Array) ? ArrayAssignment.new(key, value) : Assignment.new(key, value))
+        value
       end
     end
     
     class Disk
       attr_accessor :volume, :device, :mode
       def self.build(contents)
-        contents.rhs.map do |disk|
-          volume, device, mode = disk.split(/,/)
-          new(volume, device, mode)
-        end unless contents.nil?
+        disks = contents[:variables].detect { |var| var.lhs == :disk }
+        unless disks.nil?
+          contents[:variables].delete(disks)
+        
+          disks.rhs.map do |disk|
+            volume, device, mode = disk.split(/,/)
+            new(volume, device, mode)
+          end unless disks.rhs.nil?
+        end
       end
       def initialize(volume, device, mode)
         @volume, @device, @mode = volume, device, mode
@@ -41,19 +51,16 @@ module XenConfigFile
         "#{lhs} = #{rhs}"
       end
     end
+    
     class ArrayAssignment < Assignment
       def to_s
         "TODO"
       end
     end
     
-    class LiteralString
+    class LiteralString < String
       def to_s; "\"#{self}\"" end
     end
 
-    module Visitor
-      class Printer
-      end
-    end
   end
 end
--- spec/xen/ast_spec.rb
+++ spec/xen/ast_spec.rb
@@ -2,7 +2,56 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe XenConfigFile::AST::ConfigFile do
   before(:all) do
-    
+    @config = XenConfigFile::AST::ConfigFile.new({:variables => []})
+  end
+  require File.dirname(__FILE__) + '/../spec_helper'
+
+  describe "[]=" do
+    before(:all) do
+      @config[:cpu_cap] = 200
+    end
+    it "should let me fetch the variables i set" do
+      @config[:cpu_cap].should == 200
+    end
+  end
+end
+
+describe XenConfigFile::AST::Assignment do
+  describe "initialize" do
+    before(:all) do
+      @ass = XenConfigFile::AST::Assignment.new(:number, 42)
+    end
+    it "should know its lhs" do
+      @ass.lhs.should == :number
+    end
+    it "should know its rhs" do
+      @ass.rhs.should == 42
+    end
+  end
+end
+
+describe XenConfigFile::AST::ArrayAssignment do
+  describe "initialize" do
+    before(:all) do
+      @ass = XenConfigFile::AST::ArrayAssignment.new(:number, [42, 'forty-two', 666])
+    end
+    it "should know its lhs" do
+      @ass.lhs.should == :number
+    end
+    it "should know its rhs" do
+      @ass.rhs.should == [42, 'forty-two', 666]
+    end
+  end
+end
+
+describe XenConfigFile::AST::LiteralString do
+  describe "initialize" do
+    before(:all) do
+      @str = XenConfigFile::AST::LiteralString.new('foo')
+    end
+    it "should quote properly" do
+      @str.to_s.should == "\"foo\""
+    end
   end
 end
 
@@ -28,16 +77,16 @@ describe XenConfigFile::AST::Disk do
     end
   end
   
-  describe "build" do
-    before(:all) do
-      @params = ["phy:/dev/ey00-data4/root-s00348,sda1,w", "phy:/dev/ey00-data4/swap-s00348,sda2,w", "phy:/dev/ey00-data4/gfs-00218,sdb1,w!"]
-      @disks = XenConfigFile::AST::Disk.build(XenConfigFile::AST::ArrayAssignment.new(:disk, @params))
-    end
-    it "should build successfully" do
-      @disks.should_not be_nil
-    end
-    it "should have three elements" do
-      @disks.should have(3).entries
-    end
-  end
+  # describe "build" do
+  #   before(:all) do
+  #     @params = ["phy:/dev/ey00-data4/root-s00348,sda1,w", "phy:/dev/ey00-data4/swap-s00348,sda2,w", "phy:/dev/ey00-data4/gfs-00218,sdb1,w!"]
+  #     @disks = XenConfigFile::AST::Disk.build(XenConfigFile::AST::ArrayAssignment.new(:disk, @params))
+  #   end
+  #   it "should build successfully" do
+  #     @disks.should_not be_nil
+  #   end
+  #   it "should have three elements" do
+  #     @disks.should have(3).entries
+  #   end
+  # end
 end
