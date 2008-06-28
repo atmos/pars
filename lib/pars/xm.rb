8da9f05caa958777e3eff8bae27fe44acf72afbd
atmos <atmos@atmos.org>
2008-06-27 20:24:45 -0600

    get rid of namespace pollution

--- lib/pars/xm.rb
+++ lib/pars/xm.rb
@@ -1,6 +1,8 @@
 require 'yaml'
 
-module Xm; end
+module Parse
+  module Xm; end
+end
 require File.dirname(__FILE__)+'/xm/info'
 require File.dirname(__FILE__)+'/xm/list'
 require File.dirname(__FILE__)+'/xm/xenstore_ls'
